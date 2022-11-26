a126379d528473c1990302aee7a8f66137c70f2e
jenna5376 <jenna5376@gmail.com>
2022-11-26 16:29:50 -0500

    updated home, whiteboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import {Link, Routes, Route} from "react-router-dom"
+<<<<<<< Updated upstream
 import Home from './pages/Home/index';
 
 
@@ -14,8 +15,19 @@ function App() {
       </nav>
       <Routes>
           <Route path = "/" element = {<Home />}/>
+=======
+import Home from './pages/Home';
+import Avatar from './pages/Avatar';
+import Canvas from './pages/Canvas';
+
+function App() {
+  return (
+      <Routes>
+          <Route path = "/"  element = {<Home />}/>
+          <Route path = "/avatar"  element = {<Avatar />}/>
+          <Route path = "/canvas"  element = {<Canvas />}/>
+>>>>>>> Stashed changes
       </Routes>
-    </>
   );
 }
 
